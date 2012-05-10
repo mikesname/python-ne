0662c082640a84b479a43b14c4c74f6ad0e5d36d
Mike Bryant <mikesname@gmail.com>
2012-05-10 15:56:12 +0100

    Moved stuff around to make repository structure less dumb

--- ne/settings.py
+++ ne/settings.py
@@ -4,6 +4,9 @@ import os
 
 PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
 
+import sys
+sys.path.insert(0, PROJECT_ROOT)
+
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
@@ -106,10 +109,10 @@ MIDDLEWARE_CLASSES = (
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
-ROOT_URLCONF = 'ne.urls'
+ROOT_URLCONF = 'urls'
 
 # Python dotted path to the WSGI application used by Django's runserver.
-WSGI_APPLICATION = 'ne.wsgi.application'
+WSGI_APPLICATION = 'wsgi.application'
 
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
